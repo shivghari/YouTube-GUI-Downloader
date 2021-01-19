97993978d5209c40b291b90dd432efd3ee25c445
Shiv Ghariwala <shiv.i.ghariwala@gmail.com>
2021-01-19 17:37:09 +0530

    Beautify The code and Remove Bugs.

--- YouTube_downloader.py
+++ YouTube_downloader.py
@@ -11,7 +11,7 @@ download_path = StringVar(root)
 youtube_link = StringVar(root)
 
 def getpath():
-    path = filedialog.askdirectory();
+    path = filedialog.askdirectory()
     download_path.set(path)
     
 def download():
@@ -24,19 +24,17 @@ def download():
     root1 = Tk()
     root1.title('Message Box')
     root1.geometry('100x100')
-    l3 = Label(root1,text = 'DownloadSuccessfully').pack()
+    l3 = Label(root1, text='DownloadSuccessfully').pack()
     
     
-    
-
-root.geometry('600x150');
+root.geometry('600x150')
 root.title('Youtube Downloader')
-url_entry = Label(root,text = 'Enter the Url').pack()
-get_url = Entry(root,width = 200,textvariable = youtube_link).pack()
-path_entry = Label(root,text = 'Enter the Path').pack()
-get_path = Entry(root,width = 200,textvariable = download_path).pack()
-btn1 = Button(root,text = 'Browse',command = getpath).pack()
-btn2 = Button(root,text = 'Download', command = download).pack()
+url_entry = Label(root, text='Enter the Url').pack()
+get_url = Entry(root, width=200, textvariable=youtube_link).pack()
+path_entry = Label(root, text='Enter the Path').pack()
+get_path = Entry(root, width=200, textvariable=download_path).pack()
+btn1 = Button(root, text='Browse', command = getpath).pack()
+btn2 = Button(root, text='Download', command=download).pack()
 
 
 root.mainloop()
